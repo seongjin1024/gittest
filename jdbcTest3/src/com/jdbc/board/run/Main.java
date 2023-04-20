ce88cad7827e373f33c0587d314c969b24fa4c5a
GDJ <GDJ@DESKTOP-C1AADGB>
2023-04-20 15:07:38 +0900

    02_bs main에 출력문 추가

--- jdbcTest3/src/com/jdbc/board/run/Main.java
+++ jdbcTest3/src/com/jdbc/board/run/Main.java
@@ -3,7 +3,8 @@ package com.jdbc.board.run;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new BoardController().mainMenu();
+		System.out.println("내가 추가한 것");
 
 	}
 
