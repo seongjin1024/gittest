4c2f17311237d5515e69b6572634fb1eb027a992
GDJ <GDJ@DESKTOP-C1AADGB>
2023-04-20 15:09:42 +0900

    03_bs main 메소드에 변수추가

--- jdbcTest3/src/com/jdbc/board/run/Main.java
+++ jdbcTest3/src/com/jdbc/board/run/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		new BoardController().mainMenu();
-		System.out.println("내가 추가한 것");
+		int age = 10;
+		System.out.println(age);
 
 	}
 
